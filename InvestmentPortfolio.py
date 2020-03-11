69c3cd5ccced5943cd95065f70a33a4a73a6debc
Atlee Hasson <hassonatlee@yahoo.com>
2020-03-11 20:58:49 +0000

    Added the InvestmentPortfolio class in InvestmentPortfolio.py

---
+++ InvestmentPortfolio.py
@@ -0,0 +1,8 @@
+class InvestmentPortfolio:
+  __buying_power = None
+
+  def __init__(self, starting_capital):
+    self.__buying_power = starting_capital
+
+  def getBuyingPower(self):
+    return self.__buying_power
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import robin_stocks
+from InvestmentPortfolio import InvestmentPortfolio
 
 robin_stocks.authentication.login(username="hassonatlee@yahoo.com", password="ChiefAt033100", by_sms=True, store_session=True)
 
 initial_investment_capital = float(input("How much money would you like to start with? "))
 
-print(f"Your account will start with ${initial_investment_capital}")
+MyPortfolio = InvestmentPortfolio(initial_investment_capital)
