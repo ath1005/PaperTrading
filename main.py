0a89e08c38d4301c2e1e308181d369a5fdfee372
Atlee Hasson <hassonatlee@yahoo.com>
2020-03-14 01:38:10 +0000

    Error handling for user login to Robinhood

--- main.py
+++ main.py
@@ -6,82 +6,83 @@ from StockOption import StockOption
 from InvestmentPortfolio import InvestmentPortfolio
 from StockOption import getOptionPrice
 
-username = input("Enter your Robinhood username: ")
-password = getpass.getpass("Enter your Robinhood password: ")
+try:
+  robin_stocks.authentication.login()
+except:
+  print("Invalid login!")
+else:
+  initial_investment_capital = float(input("How much money would you like to start with? "))
 
-#robin_stocks.authentication.login(username, password, by_sms=True, store_session=True)
+  MyPortfolio = InvestmentPortfolio(initial_investment_capital)
 
-initial_investment_capital = float(input("How much money would you like to start with? "))
+  def printHelp():
+    print("-h: Print list of commands")
+    print("-ticker name: Print the name of the company associated with the given stock ticker")
+    print("-option price: Get the current price of 1 option contract with the given information")
+    print("-buy option: Purchase option contract(s)")
+    print("-buying power: Print the current buying power in the Investment Portfolio")
+    print("-q: Quit the program")
 
-MyPortfolio = InvestmentPortfolio(initial_investment_capital)
+  running = True
+  while running:
+    userInput = input("Please enter a command, or type -h for help: ")
 
-def printHelp():
-  print("-h: Print list of commands")
-  print("-ticker name: Print the name of the company associated with the given stock ticker")
-  print("-option price: Get the current price of 1 option contract with the given information")
-  print("-buy option: Purchase option contract(s)")
-  print("-buying power: Print the current buying power in the Investment Portfolio")
-  print("-q: Quit the program")
+    if(userInput == "-h"):
+      printHelp()
 
-running = True
-while running:
-  userInput = input("Please enter a command, or type -h for help: ")
+    elif(userInput == "-ticker name"):
+      ticker = input("Enter stock ticker: ")
+      stock_name = robin_stocks.stocks.get_name_by_symbol(ticker)
+      if(stock_name == ""):
+        print("Invalid stock ticker!")
+      else:
+        print(stock_name)
 
-  if(userInput == "-h"):
-    printHelp()
+    elif(userInput == "-option price"):
+      option_ticker = input("Stock ticker: ")
+      expiration_date = input("Expiration date: ")
+      strike_price = input("Strike price: ")
+      option_type = input("Option type:")
+      option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
+      print(f"${round(option_price * 100)}")
 
-  elif(userInput == "-ticker name"):
-    ticker = input("Enter stock ticker: ")
-    stock_name = robin_stocks.stocks.get_name_by_symbol(ticker)
-    if(stock_name == ""):
-      print("Invalid stock ticker!")
-    else:
-      print(stock_name)
+    elif(userInput == "-buy option"):
+      option_ticker = input("Stock ticker: ")
+      expiration_date = input("Expiration date: ")
+      strike_price = input("Strike price: ")
+      option_type = input("Option type: ")
+      quantity = input("Number of contracts: ")
+      option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
+      option = StockOption(option_ticker, expiration_date, strike_price, option_type)
+      if(MyPortfolio.buyOption(option, option_price, quantity)):
+        print("Transaction successful!")
+      else:
+        print("Transaction unsuccessful: \nYou do not have enough buying power to purchase the requested number of option contracts")
+        print(f"Buying power: ${MyPortfolio.getBuyingPower()}")
 
-  elif(userInput == "-option price"):
-    option_ticker = input("Stock ticker: ")
-    expiration_date = input("Expiration date: ")
-    strike_price = input("Strike price: ")
-    option_type = input("Option type:")
-    option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
-    print(f"${round(option_price * 100)}")
-
-  elif(userInput == "-buy option"):
-    option_ticker = input("Stock ticker: ")
-    expiration_date = input("Expiration date: ")
-    strike_price = input("Strike price: ")
-    option_type = input("Option type: ")
-    quantity = input("Number of contracts: ")
-    option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
-    option = StockOption(option_ticker, expiration_date, strike_price, option_type)
-    if(MyPortfolio.buyOption(option, option_price, quantity)):
-      print("Transaction successful!")
-    else:
-      print("Transaction unsuccessful: \nYou do not have enough buying power to purchase the requested number of option contracts")
-      print(f"Buying power: ${MyPortfolio.getBuyingPower()}")
-
-  elif(userInput == "-sell option"):
-    option_ticker = input("Stock ticker: ")
-    expiration_date = input("Expiration date: ")
-    strike_price = input("Strike price: ")
-    option_type = input("Option type: ")
-    quantity = input("Number of contracts: ")
-    option = StockOption(option_ticker, expiration_date, strike_price, option_type)
-    option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
-    if(MyPortfolio.sellOption(option, option_price, quantity)):
-      print("Transaction successful!")
-    else:
-      print(f"Transaction unsuccessful: \nYou do not have own enough contracts of: {option}")
+    elif(userInput == "-sell option"):
+      option_ticker = input("Stock ticker: ")
+      expiration_date = input("Expiration date: ")
+      strike_price = input("Strike price: ")
+      option_type = input("Option type: ")
+      quantity = input("Number of contracts: ")
+      option = StockOption(option_ticker, expiration_date, strike_price, option_type)
+      option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
+      if(MyPortfolio.sellOption(option, option_price, quantity)):
+        print("Transaction successful!")
+      else:
+        print(f"Transaction unsuccessful: \nYou do not have own enough contracts of: {option}")
 
 
-  elif(userInput == "-p"):
-    MyPortfolio.printOptions()
+    elif(userInput == "-p"):
+      MyPortfolio.printOptions()
 
-  elif(userInput == "-buying power"):
-    print(f"${MyPortfolio.getBuyingPower()}")
+    elif(userInput == "-buying power"):
+      print(f"${MyPortfolio.getBuyingPower()}")
 
-  elif(userInput == "-portfolio value"):
-    print(f"Portfolio value: ${MyPortfolio.getPortfolioValue()}")
+    elif(userInput == "-portfolio value"):
+      print(f"Portfolio value: ${MyPortfolio.getPortfolioValue()}")
 
-  elif(userInput == "-q"):
-    running = False
+    elif(userInput == "-q"):
+      robin_stocks.authentication.logout()
+      running = False
