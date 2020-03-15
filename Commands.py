536f159ed4d9dfa03ab3aab243cf22a7cdaaeb4a
Atlee Hasson <hassonatlee@yahoo.com>
2020-03-15 02:19:48 +0000

    Added documentation and minor fixes to Commands.py

--- Commands.py
+++ Commands.py
@@ -1,17 +1,47 @@
 import robin_stocks
 
 def printHelp():
-    print("-h: Print list of commands")
-    print("-ticker name: Print the name of the company associated with the given stock ticker")
-    print("-option price: Get the current price of 1 option contract with the given information")
-    print("-buy option: Purchase option contract(s)")
-    print("-buying power: Print the current buying power in the Investment Portfolio")
-    print("-q: Quit the program")
+  """
+  Prints a help menu with all defined commands and a description for each
+  """
+  print("-h: Print list of commands")
+  print("-ticker name: Print the name of the company associated with the given stock ticker")
+  print("-option price: Get the current price of 1 option contract with the given information")
+  print("-buy option: Purchase option contract(s)")
+  print("-buying power: Print the current buying power in the Investment Portfolio")
+  print("-q: Quit the program")
 
-def getStockTicker(ticker):
+def getStockName(ticker):
+  """
+  Gets the name of the stock associated with the given ticker
+
+  Returns
+  -------
+  str
+    stock name
+  """
   return robin_stocks.stocks.get_name_by_symbol(ticker)
 
 def getOptionPrice(stock_ticker, expiration_date, strike_price, option_type):
+  """
+  Gets the current price of a given option
+
+  Parameters
+  ----------
+  stock_ticker : str
+    stock ticker associated with the option
+  expiration_date : str
+    expiration date of the contract (YYYY-MM-DD)
+  strike_price : float
+    strike price of the option
+  option_type : str
+    call or put
+
+  Returns
+  -------
+  bool or float
+    price of option if it exists, False otherwise
+  """
   price = robin_stocks.options.get_option_market_data(stock_ticker, expiration_date, strike_price, option_type, 'adjusted_mark_price')
   if price != None:
     return float(price)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import time
 from StockOption import StockOption
 from InvestmentPortfolio import InvestmentPortfolio
-from Commands import printHelp, getStockTicker, getOptionPrice
+from Commands import printHelp, getStockName, getOptionPrice
 
 try:
   robin_stocks.authentication.login()
@@ -23,7 +23,7 @@ else:
 
     elif(userInput == "-ticker name"):
       ticker = input("Enter stock ticker: ")
-      stock_name = getStockTicker(ticker)
+      stock_name = getStockName(ticker)
       if(stock_name == ""):
         print("Invalid stock ticker!")
       else:
@@ -34,7 +34,7 @@ else:
       expiration_date = input("Expiration date: ")
       strike_price = input("Strike price: ")
       option_type = input("Option type: ")
-      if(getStockTicker(option_ticker) == ""):
+      if(getStockName(option_ticker) == ""):
         print("Invalid stock ticker!")
       else:
         option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
@@ -46,7 +46,7 @@ else:
       strike_price = input("Strike price: ")
       option_type = input("Option type: ")
       quantity = input("Number of contracts: ")
-      if(getStockTicker(option_ticker) == ""):
+      if(getStockName(option_ticker) == ""):
         print("Invalid stock ticker!")
       else:
         option_price = getOptionPrice(option_ticker, expiration_date, strike_price, option_type)
@@ -64,7 +64,7 @@ else:
       strike_price = input("Strike price: ")
       option_type = input("Option type: ")
       quantity = input("Number of contracts: ")
-      if(getStockTicker(option_ticker) == ""):
+      if(getStockName(option_ticker) == ""):
         print("Invalid stock ticker!")
       else:
         option = StockOption(option_ticker, expiration_date, strike_price, option_type)
